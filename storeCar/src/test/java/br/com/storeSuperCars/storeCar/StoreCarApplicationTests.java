1af546e68af689a336d76609ec285245f1b2d403
icar0s <icarooliveira_23@hotmail.com>
2023-10-09 19:47:31 -0300

    first commit initialize spring project

---
+++ storeCar/src/test/java/br/com/storeSuperCars/storeCar/StoreCarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.storeSuperCars.storeCar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

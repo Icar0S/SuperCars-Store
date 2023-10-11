df6c2ed1f7d60b37c3f34828eec8b12d61151c4b
icar0s <icarooliveira_23@hotmail.com>
2023-10-10 21:20:26 -0300

    adding first table in database h2

--- storeCar/src/main/java/br/com/storeSuperCars/storeCar/user/UserModel.java
+++ storeCar/src/main/java/br/com/storeSuperCars/storeCar/user/UserModel.java
@@ -1,26 +1,42 @@
 package br.com.storesupercars.storecar.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity(name = "tb_users")
 public class UserModel {
 
+  @Id
+  @GeneratedValue(generator = "UUID")
+  private UUID id;
+
+  @Column(name = "nome")
   private String name;
   private String email;
+  @Column(name = "idade")
   private Number age;
+  @Column(name = "senha")
   private String password;
 
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+
   // private Date birthDay;
+  // @Column(name = "telefone")
   // private Number phone;
+  // @Column(name = "cidade")
   // private String city;
 
   // Construtor
-  // public UserModel(String name, String email, Number age, String password) {
-  // this.name = name;
-  // this.email = email;
-  // this.age = age;
-  // this.password = password;
-  // }
 
   // @Override
   // public String toString() {

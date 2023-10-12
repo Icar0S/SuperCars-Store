ffefd0160c8a43abdffc0cb4be0fc84bc76a39bb
icar0s <icarooliveira_23@hotmail.com>
2023-10-12 19:28:24 -0300

    adding validations for register cars

--- storeCar/src/main/java/br/com/storeSuperCars/storeCar/cars/CarController.java
+++ storeCar/src/main/java/br/com/storeSuperCars/storeCar/cars/CarController.java
@@ -1,8 +1,11 @@
 package br.com.storesupercars.storecar.cars;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,13 +21,24 @@ public class CarController {
   private ICarRepository carRepository;
 
   @PostMapping("/")
-  public CarModel create(@RequestBody CarModel carModel, HttpServletRequest request) {
+  public ResponseEntity create(@RequestBody CarModel carModel, HttpServletRequest request) {
 
     var idUser = request.getAttribute("idUser");
     carModel.setIdUser((UUID) idUser);
-    var car = this.carRepository.save(carModel);
 
-    return car;
+    var currentDate = LocalDateTime.now();
+    if (currentDate.isAfter(carModel.getStartAt()) || currentDate.isAfter(carModel.getSoldAt())) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+          .body("Data de cadastro / data de venda deve ser maior que a data atual.");
+    }
+
+    if (carModel.getStartAt().isAfter(carModel.getSoldAt())) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+          .body("Data de cadastro deve ser menor que a data de venda.");
+    }
+
+    var car = this.carRepository.save(carModel);
+    return ResponseEntity.status(HttpStatus.OK).body(car);
   }
 
 }

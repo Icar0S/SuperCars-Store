0fb4912041caaad7341034d646136787e94e4b19
icar0s <icarooliveira_23@hotmail.com>
2023-10-12 19:39:27 -0300

    adding route listing cars user

--- storeCar/src/main/java/br/com/storeSuperCars/storeCar/cars/ICarRepository.java
+++ storeCar/src/main/java/br/com/storeSuperCars/storeCar/cars/ICarRepository.java
@@ -1,9 +1,11 @@
 package br.com.storesupercars.storecar.cars;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ICarRepository extends JpaRepository<CarModel, UUID> {
 
+  List<CarModel> findByIdUser(UUID idUser);
 }
